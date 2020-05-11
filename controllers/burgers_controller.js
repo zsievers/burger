835da7c52a530654819c6455a13f6be4c834bf0b
Zach <zjsievers@gmail.com>
2020-05-10 22:42:59 -0400

    adjusted routes// complete and working

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,9 +6,9 @@ var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 
-
+// GET REQUEST
 router.get("/", function (req, res) {
-  burger.all(function (data) {
+  burger.selectAll(function (data) {
     var hbsObject = {
       burgers: data,
     };
@@ -17,48 +17,22 @@ router.get("/", function (req, res) {
   });
 });
 
-router.post("/api/burgers", function (req, res) {
-  burger.create(
-    ["burger_name", "devoured"],
-    [req.body.burger_name, req.body.devoured],
-    function (result) {
+// POST REQUEST
+router.post("/insertOne", function (req, res) {
+  burger.insertOne(req.body.burger_name, function () {
         // send back id of new post
-      res.json({ id: result.insertId });
-    }
-  );
-});
-
-router.put("/api/burgers/:id", function(req,res) {
-    var condition = "id = " + req.params.id;
-
-    console.log("condition", condition);
-
-    burger.update({ 
-        devoured: req.body.devoured
-    }, condition, function(result){
-        if (result.changedRows === 0) {
-            // if no rows were changed, id must not exist so 404
-            return res.status(404).end();
-        } else {
-            res.status(200).end();
-        }
+      res.redirect("/");
     });
 });
 
-router.delete("/api/burgers/:id", function(req,res) {
-    var condition = "id = " + req.params.id;
-    console.log("condition", condition);
 
-    burger.delete(condition, function(results){
-        if (result.affectedRows === 0) {
-            // if no rows were affected, the id must not exist, so 404
-            return res.status(404).end();
-        } else {
-            res.status(200).end();
-        }
+router.post("/updateOne", function(req,res) {
+    var id = req.body.id
+ 
+    burger.updateOne(id, function(){
+       res.redirect("/");
     });
 });
-
 
 // export routes to server.js
 module.exports = router;
